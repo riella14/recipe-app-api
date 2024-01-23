f0807bcaa03b3836f65db93277e67fdb99d37542
riella14 <ricafrancisco17@gmail.com>
2024-01-23 15:26:35 +0800

    Install PostgreSQL db adaptor

---
+++ app/app/calc.py
@@ -0,0 +1,12 @@
+"""
+Calculator functions
+"""
+
+
+def add(x, y):
+    """Add x and y to return result."""
+    return x + y
+
+def subtract(x, y):
+    """Subtract x from y to return result."""
+    return y - x
