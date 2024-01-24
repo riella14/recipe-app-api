d898a2bc079fa78d33ee6fae7119cab3f36135ed
riella14 <ricafrancisco17@gmail.com>
2024-01-24 14:17:54 +0800

    Add superuser support

--- app/core/models.py
+++ app/core/models.py
@@ -1,7 +1,6 @@
 """
 Database models.
 """
-
 from django.db import models
 from django.contrib.auth.models import (
     AbstractBaseUser,
@@ -19,6 +18,15 @@ class UserManager(BaseUserManager):
             raise ValueError('User must have an email address.')
         user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password):
+        """Create and return a new superuser."""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
         user.save(using=self._db)
 
         return user
